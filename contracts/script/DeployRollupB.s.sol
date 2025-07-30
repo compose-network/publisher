602844166f2b628930ceb3a178dd22617430d70d
Riccardo Persiani <riccardo@ssvlabs.io>
2025-07-30 16:08:41 +0200

    chore(contracts): update script naming

--- contracts/script/DeployRollupB.s.sol
+++ contracts/script/DeployRollupB.s.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.30;
 
-import { Script, console } from "forge-std/Script.sol";
+import { Script } from "forge-std/Script.sol";
 import { DeployAll } from "./DeployAll.sol";
 
 contract DeployAllRollup is Script, DeployAll {
     function run() external {
         if (block.chainid != 22222) {
-            revert("This script is only for Ayaz Rollup with L1 Hoodi");
+            revert("This script is only for Rollup B with L1 Hoodi");
         }
 
         string memory finalJson = _deployAll(
-            vm.readFile("script/config/rollup-hoodi.json")
+            vm.readFile("script/config/rollup-b.json")
         );
 
-        vm.writeJson(finalJson, "artifacts/deploy-rollup-hoodi.json");
+        vm.writeJson(finalJson, "artifacts/deploy-rollup-b.json");
     }
 }
