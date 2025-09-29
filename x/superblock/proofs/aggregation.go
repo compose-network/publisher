9ca971190a37be33f37f6a45b1dd686f0c7730fd
ayaz-ssvlabs <ayaz@ssvlabs.io>
2025-09-29 19:23:28 +0500

    Return aggVkey

--- x/superblock/proofs/aggregation.go
+++ x/superblock/proofs/aggregation.go
@@ -91,5 +91,6 @@ type AggregationProofData struct {
 	ChainID            uint32             `json:"chain_id"`
 	AggregationOutputs AggregationOutputs `json:"aggregation_outputs"`
 	CompressedProof    PublicValueBytes   `json:"compressed_proof"`
+	AggVKey            common.Hash        `json:"agg_v_key"`
 	MailboxInfo        MailboxInfoStruct  `json:"mailbox_info"`
 }
